b7410303404d38684e87e13e9478447c350934cf
valeri1383 <valeri1383@gmail.com>
2021-04-21 18:13:18 +0100

    Delete System_Split/project/hardware directory

--- System_Split/project/hardware/power_hardware.py
+++
@@ -1,8 +0,0 @@
-#from Personal_Projects.System_Split.project.hardware.hardware import Hardware
-from project.hardware.hardware import Hardware
-
-
-class PowerHardware(Hardware):
-    def __init__(self,  name, capacity, memory):
-        super().__init__(name, 'Power', int(capacity * 0.25), int(memory * 1.75))
-
