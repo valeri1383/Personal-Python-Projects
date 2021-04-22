7d6e3498566d02ab7a540984593ce68c6343197f
valeri1383 <valeri1383@gmail.com>
2021-04-22 15:17:09 +0100

    Add files via upload

---
+++ Hotel_Everland/project/main.py
@@ -0,0 +1,31 @@
+from projec.trooms.young_couple import YoungCouple
+from project.rooms.young_couple_with_children import YoungCoupleWithChildren
+from project.people.child import Child
+from project.everland import Everland
+
+# from Personal_Projects.Hotel_Everland.project.rooms.young_couple import YoungCouple
+# from Personal_Projects.Hotel_Everland.project.rooms.young_couple_with_children import YoungCoupleWithChildren
+# from Personal_Projects.Hotel_Everland.project.people.child import Child
+# from Personal_Projects.Hotel_Everland.project.everland import Everland
+
+everland = Everland()
+
+def test_one():
+    young_couple = YoungCouple("Johnsons", 150, 205)
+
+    child_one = Child(5, 1, 2, 1)
+    child_two = Child(3, 2)
+    young_couple_with_children = YoungCoupleWithChildren("Peterson", 600, 520, child_one, child_two)
+
+    everland.add_room(young_couple)
+    everland.add_room(young_couple_with_children)
+
+    print(everland.get_monthly_consumptions())
+    print(everland.pay())
+    print(everland.status())
+
+
+if __name__ == "__main__":
+    test_one()
+
+
